2d0f49b87d7e17d96632e9f8f07ee7871dac1bcf
reeh <reeh@reeh.linuxextreme.com.br>
2013-11-21 11:03:36 -0200

    Change to operation with fabric

---
+++ fabfile.py
@@ -0,0 +1,39 @@
+from fabric.api import *
+from getpass import getpass
+
+
+def get_info(root=False):
+  try:
+    info = {}
+    info['username'] = raw_input('username: ')
+    info['password'] = getpass('password: ')
+    if root:
+      info['pass_root'] = getpass('root password: ')
+    info['command'] = raw_input('Insert command to execute: ')
+  except KeyboardInterrupt:
+      exit(1)
+  return info
+  
+def run_as_root_with_su():
+  info = get_info(True)
+  su(info['username'],info['password'], info['command'],info['pass_root'])
+
+def run_as_root_with_sudo():
+  info = get_info()
+  env.user = info['username']
+  env.password = info['password']
+  sudo('%s' % info['command'])
+
+def run_as_user_common():
+  info = get_info()
+  with settings(user='%s' % info['username'], password='%s' % info['password']):
+    sudo('%s' % info['command'])
+
+def tofile(filename):
+  env.hosts = open('%s' % filename, 'r').readlines()
+
+def su(username,password,command,pass_root):
+  with settings(user='%s' % username, password='%s' % password):
+    run('whoami')
+    with settings(user='root',password='%s' % pass_root, sudo_prefix="su -c", sudo_prompt="Password:"):
+      sudo('%s' % command)
